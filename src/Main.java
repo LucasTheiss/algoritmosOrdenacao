83d8228144d2638bb643782d729a6b3934fac9d5
LucasTheiss <lucas555andriani@gmail.com>
2025-10-23 12:18:28 -0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] vetor1= {12, 18, 9, 25, 17, 31, 22, 27, 16, 13, 19, 23, 20, 30, 14, 11, 15, 24, 26, 28};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int[] vetor2 = {5, 7, 9, 10, 12, 14, 15, 17, 19, 21, 22, 23, 24, 25, 27, 28, 29, 30, 31, 32};
+
+        int[] vetor3 = {99, 85, 73, 60, 50, 40, 35, 30, 25, 20, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6};
+
+        int l = 20; // tamanho das listas
+
+        System.out.println("\nSortings com o vetor1");
+        Sort.gnomeSort(l, vetor1);
+        Sort.cocktailSort(l, vetor1);
+        Sort.combSort(l, vetor1);
+        Sort.bubbleSort(l, vetor1);
+        Sort.bucketSort(l, vetor1);
+        Sort.selectionSort(l, vetor1);
+
+        System.out.println("\nSortings com o vetor2");
+        Sort.gnomeSort(l, vetor2);
+        Sort.cocktailSort(l, vetor2);
+        Sort.combSort(l, vetor2);
+        Sort.bubbleSort(l, vetor2);
+        Sort.bucketSort(l, vetor2);
+        Sort.selectionSort(l, vetor2);
+
+        System.out.println("\nSortings com o vetor3");
+        Sort.gnomeSort(l, vetor3);
+        Sort.cocktailSort(l, vetor3);
+        Sort.combSort(l, vetor3);
+        Sort.bubbleSort(l, vetor3);
+        Sort.bucketSort(l, vetor3);
+        Sort.selectionSort(l, vetor3);
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,17 +1,54 @@
 public class Sort {
-    void gnomeSort(int size, int[] l){
+    static void gnomeSort(int size, int[] l){
+        // Ω(n) - O(n ** n)
         int i = 0;
+        int iter = 0;
         while (i < size) {
-            if (i == 0 || l[i-1] ≤ l[i]) i++;
-		    else {int tmp = l[i]; l[i] = l[i-1]; l[--i] = tmp;}
+            if (i == 0 || l[i-1] <= l[i]) i++;
+		    else {
+                int tmp = l[i];
+                l[i] = l[i-1];
+                l[--i] = tmp;
+            }
+            iter++;
         }
+        System.out.println("GnomeSort - nº de iterações: " + iter);
     }
 
-    void bucketSort(int size, int[] l){
+    static void bucketSort(int size, int[] l){}
 
+    static void combSort(int size, int[] l){
+        // Ω(1) - O(n)
+        boolean sorted = false;
+        int gap = size, iter = 0, i, sm, aux;
+
+        while (!sorted) {
+            iter++;
+            gap = (int) (gap/1.3);
+
+            if (gap <= 1) {
+                gap = 1;
+                sorted = true;
+            }
+
+            for (i = 0; i < size - gap; i ++) {
+                iter++;
+                sm = gap + i;
+                if (l[i] > l[sm]) {
+                    aux = l [sm];
+                    l[sm] = l[i];
+                    l[i] = aux;
+                    sorted = false;
+                }
+            }
+        }
+        System.out.println("CombSort - nº de iterações: " + iter);
     }
 
-    void combSort(int size, int[] l){
+    static void bubbleSort(int size, int[] l){}
 
-    }
+    static void selectionSort(int size, int[] l){}
+
+    static void cocktailSort(int size, int[] l){}
+
 }
